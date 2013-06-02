2b8dd3e5f3426db27d0ba3273996d048d94390f3
Samat K Jain <samat@samat.org>
2013-06-02 16:35:25 -0600

    Generate GeoJSON and map w/ Folium

--- bikedata2json.py
+++ bikedata2json.py
@@ -10,6 +10,8 @@ from __future__ import print_function, unicode_literals
 import json
 import sys
 
+import folium
+
 import bikepathparse
 import pathinsquare
 
@@ -27,16 +29,35 @@ def bikedata2json(bikepathfile, zonefile, jsonfile):
     # Write JSON file with square name, path count, normalized count, square boundary coordinates
     ofile = open(jsonfile,'w')
 
+    geojson_tmpl = {'type': 'Feature',
+                    'id': None, # will be square no.
+                    'properties': {}, # various properties (numbers of paths, etc)
+                    'geometry': {} }
+
+
     # Output data structure
     out_ds = []
     
     for s in squares:
-        tempdict = {}
-        tempdict['Label'] = s[0]
-        tempdict['paths'] = s[1]
-        tempdict['pathsnorm'] = s[1]/float(maxcount)
-        tempdict['coordinates'] = s[2]
-        out_ds.append(tempdict)
+        t = geojson_tmpl.copy()
+        t['id'] = s[0]
+
+        properties = {}
+        properties['paths'] = s[1]
+        properties['pathsnorm'] = s[1]/float(maxcount)
+        t['properties'] = properties
+
+        coordinates = s[2]
+        # Create a closed GeoJSON polygon by re-adding 1st point again as the
+        #  last point
+        coordinates.append(coordinates[0])
+                                           
+        polygon_feature = {'type': 'Polygon',
+                           'coordinates': [] }
+        polygon_feature['coordinates'] = [coordinates]
+        t['geometry'] = polygon_feature
+
+        out_ds.append(t)
 
     ofile.write(json.dumps(out_ds, indent=4, separators=(',', ': ')))
     ofile.close()
@@ -48,8 +69,16 @@ def main():
 
     bikepathfile = open(sys.argv[1])
     zonefile = open(sys.argv[2])
+    json_path = sys.argv[3]
 
-    bikedata2json(bikepathfile, zonefile, sys.argv[3])
+    bikedata2json(bikepathfile, zonefile, json_path)
+
+    # Create map
+    abq_centerpoint = [35.0841034, -106.6509851]
+    map = folium.Map(location=abq_centerpoint)
+    map.geo_json(geo_path=json_path,
+                 fill_color='YlGn', fill_opacity=0.7, line_opacity=0.2,)
+    map.create_map(path="map.html")
 
 if __name__ == '__main__':
     main()
